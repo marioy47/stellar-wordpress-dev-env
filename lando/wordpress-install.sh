ef928f3f42cc7d3630ca76caef3e436708fd4f55
Mario Yepes <marioy47@gmail.com>
2022-11-30 10:36:44 -0500

    Adding redis

--- lando/wordpress-install.sh
+++ lando/wordpress-install.sh
@@ -9,8 +9,15 @@ fi
 
 if [ ! -f ${LANDO_WEBROOT}/wp-config.php ]; then
     sleep 10
-    wp config create --path=${LANDO_WEBROOT} --dbhost=database.${LANDO_APP_NAME}.internal --dbname=wordpress --dbuser=wordpress --dbpass=wordpress
-    wp core multisite-install --path=${LANDO_WEBROOT} --subdomains --title="${LANDO_APP_PROJECT}" --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email --url="$URL"
+    wp config create --path=${LANDO_WEBROOT} --dbhost=database.${LANDO_APP_NAME}.internal --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --extra-php <<PHP
+/* Lando custom config */
+define( 'WP_DEBUG', true );
+define( 'WP_REDIS_HOST', 'redis.${LANDO_APP_NAME}.internal' );
+PHP
 fi
+
+wp --path=${LANDO_WEBROOT} core multisite-install --subdomains --title="${LANDO_APP_PROJECT}" --admin_user=admin --admin_password=password --admin_email=admin@example.com --skip-email --url="$URL"
+wp --path=${LANDO_WEBROOT} plugin install redis-cache --activate
+wp --path=${LANDO_WEBROOT} redis enable
 
 echo "Go to $URL/wp-admin/ with User admin and password password"
